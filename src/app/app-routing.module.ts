6c95504b0fd6420772fa92c87f3c33c1a6d26728
plongo <paullongo@outlook.com>
2019-04-17 13:08:27 -0400

    deploying to heroku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { ClientsComponent } from "./clients/clients.component";
 import {ClientDetailComponent} from "./client-detail/client-detail.component";
 import {ClientAddComponent} from "./client-add/client-add.component";
-import {RegisterComponent} from "./register/register.component";
 import {LoginComponent} from "./login/login.component";
 
 
@@ -11,7 +10,6 @@ const routes: Routes = [
   {path: '', component: ClientsComponent},
   {path: 'clients/:id', component: ClientDetailComponent},
   {path: 'client-add', component: ClientAddComponent},
-  {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent}
 ];
 
