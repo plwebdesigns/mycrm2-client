ac7f58be86507b503621b98f77d48f4420ab0818
plongo <paullongo@outlook.com>
2019-04-23 11:02:25 -0400

    https only now

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -12,7 +12,7 @@ export class ClientsComponent implements OnInit {
 
   clients: Client[];
   links: [];
-  page: string = 'http://74.208.150.203/api/clients?page=1'; // Pagination variable
+  page: string = '74.208.150.203/api/clients?page=1'; // Pagination variable
   isLoading: boolean = true;
 
 
--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -17,7 +17,7 @@ let httpOptions = {
 })
 export class LoginService {
 
-  private API_URL = 'http://74.208.150.203/api/users/login';
+  private API_URL = '74.208.150.203/api/users/login';
 
 
   constructor(private http: HttpClient) { }
